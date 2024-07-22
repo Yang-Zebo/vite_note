0919c08e6197effcae4614890af719acd51062d6
yangzebo <1282258957@qq.com>
2024-07-22 15:31:20 +0800

    fix

---
+++ docs/.vitepress/config/navConfig.js
@@ -0,0 +1,67 @@
+ const navConfig = [
+  {
+    text: 'JavaScript',
+    items: [
+      {
+        text: '基础语法',
+        link: '/guide/JavaScript/JiChu/RuMen/00'
+      },
+      {
+        text: '基础对象',
+        link: '/guide/JavaScript/DuiXiang/BiaoZhun/00'
+      },
+      {
+        text: '要点难点',
+        link: '/guide/JavaScript/TiShen/00'
+      },
+      {
+        text: 'WebApi',
+        link: '/guide/JavaScript/WebApi/Dom/00'
+      },
+      {
+        text: 'TypeScript',
+        link: '/guide/JavaScript/Ts/JiChu/00'
+      }
+    ]
+  },
+  {
+    text: '后端相关',
+    items: [
+      {
+        text: 'NodeJS',
+        link: '/guide/NodeJs/NodeApi/01/01_00'
+      },
+      {
+        text: 'SQL',
+        link: '/guide/NodeJs/SQL/01/01_00'
+      }
+    ]
+  },
+  {
+    text: '前端框架',
+    items: [
+      {
+        text: 'Vue',
+        link: '/guide/Frame/Vue/01/01_00'
+      },
+      {
+        text: 'React',
+        link: '/guide/Frame/React/01/01_00'
+      }
+    ]
+  },
+  {
+    text: '其他要点',
+    items: [
+      {
+        text: '补充与拓展',
+        link: '/guide/Others/Expand/00'
+      },
+      {
+        text: 'Webpack',
+        link: '/guide/Others/Webpack/00'
+      }
+    ]
+  }
+]
+ export default navConfig
